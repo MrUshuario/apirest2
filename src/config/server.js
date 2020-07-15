dca3435519065859c429f19a4e3dd9a07062d0cb
Mr.Usher <lenonpoma@gmail.com>
2020-07-15 17:50:42 -0500

    arreglado los views

--- src/config/server.js
+++ src/config/server.js
@@ -8,7 +8,7 @@ const app= express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs'); // plantilal de npm ejs
-app.set('portada', path.join(__dirname, '../app/views')); // vistas html concatena
+app.set('views', path.join(__dirname, '../app/views')); // vistas html concatena
 // Middleware
 app.use(bodyParser.urlencoded({extended: false})); // pasar datos nomas BORRAR SI SE COMPLICA LAS COSAS
 
