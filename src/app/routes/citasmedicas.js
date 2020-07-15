111b8b0c37c522bc9a1279d06a79326f881048a6
Mr.Usher <lenonpoma@gmail.com>
2020-07-15 18:18:39 -0500

    terminado ejemplo comenzar con el trabajo

--- src/app/routes/citasmedicas.js
+++ src/app/routes/citasmedicas.js
@@ -9,6 +9,7 @@ const connection = dbConnection();
 app.get('/', (req, res) => {
 
 connection.query('Select * FROM citas', (err,result) => {
+	console.log(result);
 res.render('portada', { // no se pone portada.ejs en con lo setting es portada a seca
    citas: result // en el body se recibe una variable citas
     }); //res.render es para renderizar
